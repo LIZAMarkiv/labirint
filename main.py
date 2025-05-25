43664ddec4fdd52087dd4c70c1e182da9d3c3912
Lpopivnyak <lizamarkiv219@gmail.com>
2025-05-25 16:24:21 +0300

    main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import pygame
 
 class BaseSprite():
@@ -13,6 +15,11 @@ class BaseSprite():
     def draw(self,window):
         window.blit(self.texture, self.hitbox)
 
+class Gold(BaseSprite):
+    def update(self):
+        pass
+
+
 class Hero(BaseSprite):
     def update(self):
         keys = pygame.key.get_pressed()
@@ -24,6 +31,25 @@ class Hero(BaseSprite):
             self.hitbox.y -= self.speed
         if keys[pygame.K_k]:
             self.hitbox.y += self.speed
+
+class Enemy(BaseSprite):
+    def __init__(self, x1, y1, texture, speed, w, h, x2, y2):
+        super().__init__(x1,y1,texture,speed, w, h)
+        self.direction = "forward"
+        self.x1 = x1
+        self.x2 = x2
+        self.y2 = y2
+
+    def update(self):
+        if self.direction == "forward":
+            self.hitbox.x += self.speed
+            if self.hitbox.x > self.x2:
+                self.direction= "back"
+        else:
+            self.hitbox.x -= self.speed
+            if self.hitbox.x < self.x1:
+                self.direction = "forward"
+
 
 class Wall():
     def __init__(self, x, y, color, w, h):
@@ -41,7 +67,9 @@ clock = pygame.time.Clock()
 
 background_img = pygame.image.load("background.jpg")
 background_img = pygame.transform.scale(background_img, [700,500])
-hero = Hero(150,111,"hero.png", 50,50,5)
+hero = Hero(150,111,"hero.png", 50,50,2)
+enemy = Enemy(534,110,"cyborg.png",50,50,1,604,110)
+gold = Gold(615,42,"treasure.png",50,50,0)
 game = True
 
 walls = [
@@ -76,12 +104,14 @@ walls = [
 
 ]
 
+pygame.mixer.init()
+pygame.mixer.music.load("jungles.ogg")
+pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(0.5)
 
-
-
-
-
-
+gold_sound = pygame.mixer.Sound("money.ogg")
+kick_sound = pygame.mixer.Sound("kick.ogg")
+text = pygame.font.Font(None, 170).render("Перемога!", True, [0, 0, 0])
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -90,12 +120,37 @@ while game:
         if event.type == pygame.MOUSEBUTTONDOWN:
             print(pygame.mouse.get_pos())
 
-
-
+    gold.update()
+    enemy.update()
     hero.update()
+
+    for wall in walls:
+        if hero.hitbox.colliderect(wall.hitbox):
+            hero.hitbox.x = 150
+            hero.hitbox.y = 111
+            kick_sound.play()
+
+
+
+
+    if hero.hitbox.colliderect(gold.hitbox):
+        window.fill([0, 123, 0])
+        window.blit(text, [50,194])
+        gold_sound.play()
+        pygame.display.flip()
+        sleep(5)
+
+    if hero.hitbox.colliderect(enemy.hitbox):
+        hero.hitbox.x = 150
+        hero.hitbox.y = 111
+        kick_sound.play()
+
     window.fill([123,123,123])
     window.blit(background_img,[0,0])
+    gold.draw(window)
     hero.draw(window)
+    enemy.draw(window)
+
     for wall in walls:
         wall.draw(window)
     pygame.display.flip()
